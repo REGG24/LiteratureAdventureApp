5b5beb5950c5cc76973465dd4295e7ef7482603b
Ruben Esau Garcia Gomez <ruben.gomez@3pillarglobal.com>
2022-04-08 13:57:29 -0500

    pagination added to employees

--- src/components/pages/Employees/AllEmployees.tsx
+++ src/components/pages/Employees/AllEmployees.tsx
@@ -4,13 +4,19 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
 import Paper from '@mui/material/Paper';
+
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 import { getAllEmployees } from '../../../services/employees.service';
 import { useEffect, useState, Fragment } from 'react';
 import { Employee } from './interfaces';
 import { Box, Button, Modal } from '@mui/material';
 import { NewEmployee } from './NewEmployee';
+import TablePaginationActions from '@mui/material/TablePagination/TablePaginationActions';
 
 const style = {
   position: "absolute" as "absolute",
@@ -24,10 +30,19 @@ const style = {
   p: 4,
 };
 
-export const AllEmployees = () => {
+const buttonStyle = {
+  maxWidth: '40px', 
+  maxHeight: '40px', 
+  minWidth: '30px', 
+  minHeight: '30px'
+};
 
+export const AllEmployees = () => {
   const [employees, setEmployees] = useState<Employee[]>([]);
   const [open, setOpen] = useState(false);
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
  
   useEffect(() => {
     async function fetchEmployees(){
@@ -39,6 +54,19 @@ export const AllEmployees = () => {
   const handleModal = () => {
     setOpen(prev => !prev);
   }
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - employees.length) : 0;
+
+  const handleChangePage = (event: any, newPage: any) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: any) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   if (!employees) {
     return <p>Loading data...</p>;
@@ -53,14 +81,18 @@ export const AllEmployees = () => {
             <TableHead>
               <TableRow>
                 <TableCell>Id</TableCell>
-                <TableCell align="right">Name</TableCell>
-                <TableCell align="right">Phone</TableCell>
-                <TableCell align="right">Address</TableCell>
-                <TableCell align="right">Salary</TableCell>
+                <TableCell align="left">Name</TableCell>
+                <TableCell align="left">Phone</TableCell>
+                <TableCell align="left">Address</TableCell>
+                <TableCell align="left">Salary</TableCell>
+                <TableCell colSpan={2} align="center" style={{width: 100}}>Actions</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {employees.map((employee) => (
+              {(rowsPerPage > 0
+                  ? employees.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  : employees
+                ).map((employee) => (
                 <TableRow
                   key={employee.ID_EMPLOYEE}
                   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -68,13 +100,49 @@ export const AllEmployees = () => {
                   <TableCell component="th" scope="row">
                     {employee.ID_EMPLOYEE}
                   </TableCell>
-                  <TableCell align="right">{employee.NAME}</TableCell>
-                  <TableCell align="right">{employee.PHONE}</TableCell>
-                  <TableCell align="right">{employee.ADDRESS}</TableCell>
-                  <TableCell align="right">{employee.SALARY}</TableCell>
+                  <TableCell align="left">{employee.NAME}</TableCell>
+                  <TableCell align="left">{employee.PHONE}</TableCell>
+                  <TableCell align="left">{employee.ADDRESS}</TableCell>
+                  <TableCell align="left">{employee.SALARY}</TableCell>
+                  <TableCell align="center" size='small'>
+                    <Button variant="contained" style={buttonStyle}>
+                      <EditIcon />
+                    </Button>
+                  </TableCell>
+                  <TableCell align="center" size='small'>
+                    <Button variant="contained" style={buttonStyle}>
+                      <DeleteIcon />
+                    </Button>
+                  </TableCell>
                 </TableRow>
               ))}
+
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
             </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                  colSpan={3}
+                  count={employees.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  SelectProps={{
+                    inputProps: {
+                      'aria-label': 'rows per page',
+                    },
+                    native: true,
+                  }}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActions}
+                />
+              </TableRow>
+            </TableFooter>
           </Table>
           <Modal
             open={open}
